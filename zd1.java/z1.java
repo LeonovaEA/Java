c31f8c49697a66ca335e5e9830364867b9d146dd
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 18:31:15 +0300

    Update z1.java

--- zd1.java/z1.java
+++ zd1.java/z1.java
@@ -1,1 +1,17 @@
+import java.util.Random;
 
+public class z1 {
+    public static void main(String[] args) {
+        Random ran = new Random();
+        int size1 = 2;
+        int size2 = 4;
+        int[][] matrix = new int[size1][size2];
+        for (int p = 0; q < size2; i++) {
+            for (int p = 0; p < size1; p++) {
+                matrix[p][q] = ran.nextInt(10);
+                System.out.print(matrix[p][q] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
