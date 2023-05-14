0e65ad35236061cc9da80094dcce15bcff51b876
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 20:53:38 +0300

    Update z13.java

--- zd13.java/z13.java
+++ zd13.java/z13.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class ZD13 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+        int num3 = s.nextInt();
+        int n = 0;
+        if(num1 < num2){
+            if(num2 < num3){
+                while(n < num3){
+                    System.out.println(num1);
+                    n++;
+                    num1 += num2;
+                }
+            }
+        }
+    }
+}
+
 
