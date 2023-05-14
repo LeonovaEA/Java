8bfee8f1142f7f8e640706846adc52a7346dff5d
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 21:39:15 +0300

    Update z26.java

--- zd26.java/z26.java
+++ zd26.java/z26.java
@@ -1,1 +1,28 @@
+import java.util.Arrays;
+
+public class ZD26 {
+    public static void main(String[] args) {
+        String palindr = palindrString("abbbcac");
+        System.out.println(palindr + "\n" + palindr.length());
+    }
+    public static String palindr(String str, int left, int right) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
+        }
+        return str.substring(left + 1, right);
+    }
+    public static String palindrString(String str) {
+        String longest = str.substring(0, 1);
+        for (int q = 0; q < str.length() - 1; q++) {
+            if (palindr(str, q, q).length() > longest.length()) {
+                longest = palindr(str, q, q);
+            }
+            if (palindr(str, q, q + 1).length() > longest.length()) {
+                longest = palindr(str, q, q + 1);
+            }
+        }
+        return longest;
+    }
+}
 
