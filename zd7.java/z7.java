f77cc79e84f921d0a50b00556260fa42579eac02
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 19:48:54 +0300

    Update z7.java

--- zd7.java/z7.java
+++ zd7.java/z7.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
 
+public class ZD7 {
+    public static void main(String[] args){
+        System.out.println("Введите число m");
+        Scanner m = new Scanner(System.in);
+        int number = m.nextInt();
+        System.out.println(Math.round(Math.sqrt(number)));
+
+    }
+}
