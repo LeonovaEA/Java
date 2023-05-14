a069ddc5071c94600376b28aeea216d030bafe94
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 21:08:21 +0300

    Update z17.java

--- zd17.java/z17.java
+++ zd17.java/z17.java
@@ -1,1 +1,16 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ZD17 {
+    public static void main(String[] args){
+        boolean linee = correctness("(((())()");
+        System.out.println(linee);
+    }
+    public static boolean correctness(String str){
+        while (str.contains("()")) {
+            str = str.replaceAll("\\(\\)", "");
+        }
+        return (str.length() == 0);
+    }
+}
 
