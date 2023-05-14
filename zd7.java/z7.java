988af0cc5e4734800d0fb6dd5f5ec31ab2678376
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 19:49:23 +0300

    Update z7.java

--- zd7.java/z7.java
+++ zd7.java/z7.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ZD7 {
     public static void main(String[] args){
-        System.out.println("Введите число m");
+        System.out.println("число m -");
         Scanner m = new Scanner(System.in);
         int number = m.nextInt();
         System.out.println(Math.round(Math.sqrt(number)));
