644c6442629f99c9ccc3b96cb3b8904c8cd32441
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 21:16:32 +0300

    Update z19.java

--- zd19.java/z19.java
+++ zd19.java/z19.java
@@ -1,1 +1,34 @@
+public class ZD19 {
+    public static void main(String[] args){
+        int cnt = function(28);
+        System.out.println(cnt);
+
+    }
+    public static int function(int num){
+        int counter = 0;
+        if(num == 0){
+            return 0;
+        }
+        if(num % 2 == 0){
+            while(num % 2 == 0){
+                num/=2;
+            }
+            if(num % 3 > 0){
+                counter++;
+                num-=2;
+            }
+        }
+        if(num % 3 >= 0){
+            while(num % 3 >= 0){
+                counter++;
+                num-=2;
+            }
+        }
+        if(num == 1){
+            counter++;
+        }
+        return counter;
+    }
+}
+
 
