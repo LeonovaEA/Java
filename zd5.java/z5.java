c5eead986303aff755dc97b772981b388637c36a
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 19:40:50 +0300

    Update z5.java

--- zd5.java/z5.java
+++ zd5.java/z5.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class ZD5 {
+    public static void main(String[] args) {
+        System.out.println("Введите числа через пробел, чтобы перемножить их на самих себя и сложить");
+        int[] list;
+        String str;
+        String[] strnew;
+        Scanner scan = new Scanner(System.in);
+        str = scan.nextLine();
+        strnew = str.split(" ");
+        list = new int[strnew.length];
+        int x = 0;
+        for (int q = 0; q < strnew.length; q++){
+            list[q] =  Integer.parseInt(strnew[q]);
+            int m = list[q] * list[q];
+            x += m;
+        }
+        System.out.println(x);
+    }
+}
