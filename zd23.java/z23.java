3ace3bd40f8492b37a66ca6d93d61f7722c0a298
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 21:27:33 +0300

    Update z23.java

--- zd23.java/z23.java
+++ zd23.java/z23.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class ZD23 {
+    public static void main(String[] args){
+        Scanner val = new Scanner(System.in);
+        int n = val.nextInt();
+        int num1 = val.nextInt();
+        int num2 = val.nextInt();
+        int count = 0;
+        for(int q = 1; q <= n;q++){
+            if(q % num1 == 0||q % num2 == 0){
+                count += q;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
 
