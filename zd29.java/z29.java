dd0564f83c38ea61d63d9f986952871a94b21fe6
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 21:50:03 +0300

    Update z29.java

--- zd29.java/z29.java
+++ zd29.java/z29.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+
+public class ZD29 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String num1 = scan.nextLine();
+        int num1_int = Integer.parseInt(num1.trim());
+        int num2 = scan.nextInt() - 1;
+        int summ = 0;
+        String[] nums = num1.split("");
+        for(int q = 0; q < nums.length; q++){
+            num2++;
+            int n = Integer.parseInt(nums[q].trim ());
+            summ += Math.pow(n,num2);
+
+        }
+        if(summ % num1_int == 0){
+
+            System.out.println(true);
+
+        } else {
+            System.out.println(false);
+
+
+        }
+
+
+
+    }
+}
+
 
