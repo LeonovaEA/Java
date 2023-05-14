22decdfec309e446b7954de8bc90a9d0391b0208
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 20:20:42 +0300

    Update z9.java

--- zd9.java/z9.java
+++ zd9.java/z9.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
 
+public class ZD9 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        String stroka1 = x.nextLine();
+        String stroka2 = x.nextLine();
+        boolean str = stroka1.endsWith(stroka2);
+        System.out.println(str);
+    }
+}
