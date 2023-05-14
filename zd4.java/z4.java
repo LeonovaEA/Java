4ef5b3ebe300cd15125eec2ae0b710541bdf22bc
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 19:38:53 +0300

    Update z4.java

--- zd4.java/z4.java
+++ zd4.java/z4.java
@@ -1,1 +1,18 @@
-
+public class ZD4 {
+    public static void main(String[] args){
+        int s = str_count("Hello", 'q');
+        int s1 = str_count("Hello", 'e');
+        int s2 = str_count("", 'm');
+        System.out.println(s + "\n" + s1 + "\n" + s2);
+    }
+    public static int str_count(String str, char letter){
+        int count = 0;
+        for(int n = 0; n < str.length(); n++){
+            char x = str.charAt(n);
+            if(x == letter) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
