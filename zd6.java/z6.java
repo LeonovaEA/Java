2a79c516651fe5e95ac2e20f3693974bd5578ec0
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-14 19:45:35 +0300

    Update z6.java

--- zd6.java/z6.java
+++ zd6.java/z6.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class ZD6 {
+    public static void main(String[] args){
+        boolean count = number(5869);
+        System.out.println(count);
+    }
+    public static boolean number(int x){
+        int q = 2;
+        while (x % q != 0){
+            q++;
+        }
+        if (q == x){
+            return true;
+        }
+        return false;
+    }
 
+}
