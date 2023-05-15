a014adad94ad462e49adbc2b8b6b0356d8229f3a
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-15 23:40:25 +0300

    Update z34.java

--- zd34.java/z34.java
+++ zd34.java/z34.java
@@ -1,1 +1,22 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ZD34 {
+    public static void main(String[] args) {
+        String input = "56 54 88 100 91 68 43 180 70";
+        String[] weights = input.split(" ");
+        Arrays.sort(weights, Comparator.comparingInt(ZD34::sumOfDigits).reversed());
+        System.out.println(String.join(" ", weights));
+    }
+        int sum = 0;
+        for (int q = 0; q < number.length(); q++) {
+            sum += Character.getNumericValue(number.charAt(q));
+        }
+        return sum;
+    }
+
+}
+
+
+
 
