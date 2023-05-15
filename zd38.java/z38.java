feb4255055b4767b7f4f7a6af1c3d4635d64cbf6
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-16 00:08:22 +0300

    Update z38.java

--- zd38.java/z38.java
+++ zd38.java/z38.java
@@ -1,1 +1,20 @@
+public class ZD38 {
+    public static void main(String[] args) {
+        String text = "Хочу каникулы,а не вот это все";
+        String[] sentences = text.split("\\.\\s*");
+
+        for (int q = 0; q < sentences.length; q++) {
+            String[] replace = sentences[q].split("\\s+");
+            for (int j = 0; j < replace.length; j++) {
+                String rest = words[j].substring(1);
+                words[j] = rest + firstLetter + "ауч";
+            }
+            sentences[q] = String.join(" ", replace) + ".";
+        }
+
+        String result = String.join(" ", sentences);
+        System.out.println(result);
+    }
+
+}
 
