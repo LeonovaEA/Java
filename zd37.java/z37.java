58a36f99bc6e8867736974559c09d7c6e46461b8
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2023-05-16 00:02:05 +0300

    Update z37.java

--- zd37.java/z37.java
+++ zd37.java/z37.java
@@ -1,1 +1,32 @@
+import java.util.List;
+import java.util.Scanner;
 
+public class ZD37 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("количество игроков: ");
+        int q = scanner.nextInt();
+        System.out.print("номер игрока (которого начинает игрy): ");
+        int start = scanner.nextInt();
+
+        List<Integer> participants = new ArrayList<>();
+        for (int l = 1; l <= q; l++) {
+            participants.add(l);
+        }
+        List<Integer> result = new ArrayList<>();
+        int count = 0;
+        int index = start - 1;
+
+        while (participants.size() > 1) {
+            count++;
+            index = (index + count) % participants.size();
+                result.add(participants.remove(index));
+                index--;
+            }
+        }
+
+        result.addAll(participants);
+        System.out.println(result);
+    }
+
+}
